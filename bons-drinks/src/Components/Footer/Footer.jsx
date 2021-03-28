7e607185471c892e2c5c91400786d4db7cefc6eb
justo-marcella <justoamarcella@gmail.com>
2021-03-27 21:01:58 -0300

    Componente Footer

---
+++ bons-drinks/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={style.footerStyle + " header "}>
+            <p>Resilia 2021 - Alguns direitos reservados</p>
+        </footer>
+    )
+}
+
+export default Footer
