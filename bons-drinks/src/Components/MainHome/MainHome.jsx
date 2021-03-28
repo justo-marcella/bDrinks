39c77c9ddf7c4758b56332ef30ab3d219e44b0f0
justo-marcella <justoamarcella@gmail.com>
2021-03-27 21:02:52 -0300

    Componente Home

---
+++ bons-drinks/src/Components/MainHome/MainHome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './MainHome.module.css';
+
+function MainHome() {
+    return (
+        
+            <section>
+                <div className={style.banner}>
+                    <h3>Bons Drinks</h3>
+                </div>
+            </section>
+            
+    )
+}
+
+export default MainHome
