55e9ee2ed75b3ed92766b64b79d068674feb3a42
justo-marcella <justoamarcella@gmail.com>
2021-03-28 21:53:07 -0300

    Ajuste da Home

--- bons-drinks/src/App.js
+++ bons-drinks/src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 import  Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import MainHome from './Components/MainHome/MainHome';
-import GetDrinks from './Components/GetDrinks/GetDrinks';
+// import MainHome from './Components/MainHome/MainHome';
+// import GetDrinks from './Components/GetDrinks/GetDrinks';
+import Sobre from './Components/Sobre/Sobre';
+import Team from './Components/Team/Team';
 // import {BrowserRouter as Router} from "react-router-dom"
 
 
@@ -12,9 +14,11 @@ function App() {
     <div className="App">
       {/* <Router> */}
         <Header className="header"/>      
-        <MainHome className="mainContents"/>
+        {/* <MainHome className="mainContents"/> */}
       {/* </Router> */}
       {/* <GetDrinks className="mainContents"/> */}
+      {/* <Sobre className="mainContents"/> */}
+      <Team className="mainContents"/>
         <Footer className="footer"/>
     </div>
   );
